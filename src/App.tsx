32fefe4734043d591b289ebb5cc490e3a82f1a0b
Liu3oIT <liutyivova995@gmail.com>
2024-11-01 03:34:58 +0200

    fix route

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import PokemonList from "./PokemonList";
+import { Route, Routes } from "react-router-dom";
 import PokemonDetails from "./PokemonDetails";
 import SearchPokemom from "./SearchPokemon";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SearchPokemom />} />
-        {/* <Route path="/pokemon" element={<PokemonList />} /> */}
-
-        <Route path="/pokemon/:name" element={<PokemonDetails />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<SearchPokemom />} />
+      <Route path="/pokemon/:name" element={<PokemonDetails />} />
+    </Routes>
   );
 };
 
